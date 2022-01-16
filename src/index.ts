c42fa5c747c3d1b36d591d3b3344f759e6d4699f
iamprincesly <iamprincesly@gmail.com>
2022-01-17 00:28:48 +0100

    Implement sorting for characters

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([1, 5, 8, 3, 4]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const charSorter = new Sorter(charactersCollection);
+console.log(charactersCollection.data);
