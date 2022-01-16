fa7ef22e87076bd7c36881b30ef1d4bdfd02e187
iamprincesly <iamprincesly@gmail.com>
2022-01-16 19:36:08 +0100

    Implment for numbers

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,21 @@
 "use strict";
-const doSomething = () => {
-    console.log('Hi there');
-};
-doSomething();
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([55, 93, 7, 82, -63, 32, 73, 90]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-const doSomething = () => {
-    console.log('Hi there');
-};
+class Sorter {
+    constructor(public collection: number[]) {}
 
-doSomething();
+    sort(): void {
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([55, 93, 7, 82, -63, 32, 73, 90]);
+sorter.sort();
+console.log(sorter.collection);
