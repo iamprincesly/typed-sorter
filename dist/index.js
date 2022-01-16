c84c29ff2474a942021d43e11b23c8192d50cb06
iamprincesly <iamprincesly@gmail.com>
2022-01-16 23:17:01 +0100

    Update Sorter to use interface

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([1, 74, 7, 2, 5, 9, 3]);
+const numbersCollection = new NumbersCollection_1.NumbersCollection([1, 5, 8, 3, 4]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const numbersCollection = new NumbersCollection([1, 74, 7, 2, 5, 9, 3]);
+const numbersCollection = new NumbersCollection([1, 5, 8, 3, 4]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
